a630738451be63d5911d532dbd1e6a66c97b9091
Jiaze Lin <peter_lin2@baylor.edu>
2023-02-09 13:29:06 -0600

    2.9

--- insect.py
+++ insect.py
@@ -6,5 +6,17 @@ def main():
 
     print("This insect can fly: ", my_insect.get_fly())
 
+    #insect fly
+    print('This insect is going to fly ten times: ')
+    for count in range(10):
+        my_insect.fly()
+
+        # Display the side of the coin that is facing up.
+        print('This insect flew:', my_insect.get_fly())
+
+
+
+
+
 
 main()
